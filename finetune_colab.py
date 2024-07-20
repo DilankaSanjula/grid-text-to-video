c11628b23458f088f72d090ca4afc0c2ca9c3a62
DilankaSanjula <dilankasanjula@gmail.com>
2024-07-20 10:50:18 +0530

    Fixed data loading

--- finetune_colab.py
+++ finetune_colab.py
@@ -19,7 +19,7 @@ def load_and_preprocess_image(file_path):
     image = image / 255.0  # Normalize to [0, 1]
     return image
 
-def load_dataset(dataset_path, batch_size=8):
+def load_dataset(dataset_path):
     def parse_function(file_path):
         image = load_and_preprocess_image(file_path)
         caption = tf.strings.split(tf.strings.regex_replace(file_path, dataset_path + '/', ''), '.')[0]
@@ -27,7 +27,6 @@ def load_dataset(dataset_path, batch_size=8):
 
     dataset = tf.data.Dataset.list_files(dataset_path + '/*.jpg')
     dataset = dataset.map(parse_function, num_parallel_calls=tf.data.AUTOTUNE)
-    dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     return dataset
 
